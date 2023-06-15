2f3392fd8f2cfdcff8d37298be13924ae6397b3d
MaksymRipnyi <99602314+MaksymRipnyi@users.noreply.github.com>
2023-06-15 17:24:08 +0300

    Добавьте файлы проекта.

---
+++ FORMA.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace C_LAB6
+{
+  partial class LB6
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.select_file = new System.Windows.Forms.Button();
+            this.upload_file = new System.Windows.Forms.Button();
+            this.file = new System.Windows.Forms.Label();
+            this.files_list = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.update_files_list = new System.Windows.Forms.Button();
+            this.play_selected = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // select_file
+            // 
+            this.select_file.BackColor = System.Drawing.Color.Silver;
+            this.select_file.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.select_file.Location = new System.Drawing.Point(259, 405);
+            this.select_file.Name = "select_file";
+            this.select_file.Size = new System.Drawing.Size(165, 23);
+            this.select_file.TabIndex = 1;
+            this.select_file.Text = "Обрати";
+            this.select_file.UseVisualStyleBackColor = false;
+            this.select_file.Click += new System.EventHandler(this.select_file_Click);
+            // 
+            // upload_file
+            // 
+            this.upload_file.BackColor = System.Drawing.Color.Thistle;
+            this.upload_file.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.upload_file.Location = new System.Drawing.Point(477, 402);
+            this.upload_file.Name = "upload_file";
+            this.upload_file.Size = new System.Drawing.Size(165, 68);
+            this.upload_file.TabIndex = 2;
+            this.upload_file.Text = "Завантажити на сервер";
+            this.upload_file.UseVisualStyleBackColor = false;
+            this.upload_file.Click += new System.EventHandler(this.upload_file_Click);
+            // 
+            // file
+            // 
+            this.file.AutoSize = true;
+            this.file.Location = new System.Drawing.Point(101, 13);
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(10, 13);
+            this.file.TabIndex = 4;
+            this.file.Text = "-";
+            // 
+            // files_list
+            // 
+            this.files_list.BackColor = System.Drawing.Color.PeachPuff;
+            this.files_list.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.files_list.FormattingEnabled = true;
+            this.files_list.Location = new System.Drawing.Point(24, 31);
+            this.files_list.Name = "files_list";
+            this.files_list.Size = new System.Drawing.Size(618, 351);
+            this.files_list.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Audio:";
+            // 
+            // update_files_list
+            // 
+            this.update_files_list.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.update_files_list.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.update_files_list.Location = new System.Drawing.Point(24, 405);
+            this.update_files_list.Name = "update_files_list";
+            this.update_files_list.Size = new System.Drawing.Size(165, 63);
+            this.update_files_list.TabIndex = 7;
+            this.update_files_list.Text = "Оновити список";
+            this.update_files_list.UseVisualStyleBackColor = false;
+            this.update_files_list.Click += new System.EventHandler(this.update_files_list_Click);
+            // 
+            // play_selected
+            // 
+            this.play_selected.BackColor = System.Drawing.Color.YellowGreen;
+            this.play_selected.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.play_selected.Location = new System.Drawing.Point(259, 445);
+            this.play_selected.Name = "play_selected";
+            this.play_selected.Size = new System.Drawing.Size(165, 23);
+            this.play_selected.TabIndex = 8;
+            this.play_selected.Text = "Відтворити";
+            this.play_selected.UseVisualStyleBackColor = false;
+            this.play_selected.Click += new System.EventHandler(this.play_selected_Click);
+            // 
+            // LB6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(667, 480);
+            this.Controls.Add(this.play_selected);
+            this.Controls.Add(this.update_files_list);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.files_list);
+            this.Controls.Add(this.file);
+            this.Controls.Add(this.upload_file);
+            this.Controls.Add(this.select_file);
+            this.Name = "LB6";
+            this.Text = "LB6 -- Korovina";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button select_file;
+    private System.Windows.Forms.Button upload_file;
+    private System.Windows.Forms.Label file;
+    private System.Windows.Forms.ListBox files_list;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button update_files_list;
+    private System.Windows.Forms.Button play_selected;
+    }
+}
+
---
+++ FORMA.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using NAudio;
+using NAudio.Wave;
+
+namespace C_LAB6
+{
+    public partial class LB6 : Form
+    {
+        public string filePath = ""; // Зберігає шлях до вибраного аудіофайлу
+        public HttpClient client = new HttpClient(); // HttpClient для взаємодії з сервером
+        public string backendUrl = "http://localhost:8080"; // URL сервера
+        private bool isPlaying = false; // Прапорець, що вказує, чи відбувається відтворення аудіофайлу
+        private WaveOut waveOut; // Об'єкт WaveOut для відтворення аудіо
+
+        private const int UdpPort = 12345; // Порт для UDP-з'єднання
+        private const string ServerIPAddress = "127.0.0.1"; // IP-адреса сервера
+        private const int Port = 12345; // Порт для UDP-з'єднання
+
+        public LB6()
+        {
+            InitializeComponent();
+        }
+
+        private void select_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3";
+            openFileDialog.Title = "Select an MP3 File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                filePath = selectedFilePath;
+                file.Text = selectedFilePath;
+            }
+        }
+
+        private void upload_file_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] fileData = File.ReadAllBytes(filePath); // Зчитування даних з аудіофайлу
+
+                ByteArrayContent fileContent = new ByteArrayContent(fileData);
+                HttpResponseMessage response = client.PostAsync($"{backendUrl}/add-audio", fileContent).Result; // Відправка POST-запиту з аудіоданими на сервер
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Audio file uploaded successfully.");
+                }
+                else
+                {
+                    Console.WriteLine($"Error uploading audio file. Status Code: {response.StatusCode}");
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found: " + ex.Message);
+            }
+        }
+
+        private async void update_files_list_Click(object sender, EventArgs e)
+        {
+            HttpResponseMessage response = await client.GetAsync($"{backendUrl}/get-audios"); // Відправка GET-запиту на сервер для отримання списку аудіофайлів
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                string[] audios = JsonSerializer.Deserialize<string[]>(json);
+
+                files_list.Items.Clear();
+
+                foreach (string audio in audios)
+                {
+                    files_list.Items.Add(audio);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error getting audios. Status Code: {response.StatusCode}");
+            }
+        }
+
+        private void HandleUdp(string FilePath)
+        {
+            using (UdpClient udpClient = new UdpClient())
+            {
+                IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(ServerIPAddress), Port);
+
+                try
+                {
+                    byte[] filePathBytes = Encoding.UTF8.GetBytes(FilePath);
+
+                    udpClient.Send(filePathBytes, filePathBytes.Length, serverEndpoint); // Відправка шляху аудіофайлу на сервер через UDP-з'єднання
+
+                    byte[] audioData = udpClient.Receive(ref serverEndpoint); // Отримання аудіоданих з сервера
+
+                    using (MemoryStream stream = new MemoryStream(audioData))
+                    {
+                        Mp3FileReader reader = new Mp3FileReader(stream);
+                        waveOut = new WaveOut();
+                        waveOut.Init(reader);
+                        waveOut.Play(); // Відтворення аудіофайлу
+
+                        // Очікування завершення відтворення
+                        while (waveOut.PlaybackState == PlaybackState.Playing)
+                        {
+                            Thread.Sleep(100);
+                        }
+
+                        waveOut.Stop();
+                        waveOut.Dispose();
+                        waveOut = null;
+                        reader.Dispose();
+                        reader = null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error occurred: " + ex.Message);
+                }
+            }
+        }
+
+        private void play_selected_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!isPlaying) // Перевірка, чи не відбувається вже відтворення аудіофайлу
+                {
+                    string selectedItem = files_list.SelectedItem?.ToString();
+
+                    if (!string.IsNullOrEmpty(selectedItem))
+                    {
+                        if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
+                        {
+                            waveOut.Stop();
+                            waveOut.Dispose();
+                            waveOut = null;
+                        }
+
+                        Thread udpThread = new Thread(() =>
+                        {
+                            isPlaying = true;
+                            HandleUdp(selectedItem); // Відтворення вибраного аудіофайлу
+                            isPlaying = false;
+                        });
+                        udpThread.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select an audio file to play.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Audio is already playing.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error can't play audio. Status Code: {ex.Message}");
+            }
+        }
+
+
+    }
+}
